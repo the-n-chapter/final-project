335d4858c592fddbcce8ee07eefa29dae1b2e316
phamk4 <khanh.b.pham@aalto.fi>
2025-02-08 21:59:45 +0200

    Title font responsive

--- src/components/main-content/Title.tsx
+++ src/components/main-content/Title.tsx
@@ -6,17 +6,17 @@ export function Title() {
   return (
     <div>
       {/* Container for title + IPA */}
-      <div className="flex flex-col md:flex-row md:items-baseline gap-1 md:gap-3 mb-4 md:mb-8">
-        <h1 className="text-7xl font-bold font-noto-serif">
-          Ig Nobelpedia
+      <div className="flex flex-col md:flex-row md:items-baseline gap-3 md:gap-5 mb-4 md:mb-8">
+        <h1 className="text-6xl md:text-8xl font-bold font-noto-serif">
+          Ig Nobelpedia.
         </h1>
-        <span className="text-sm text-gray-600 md:-mb-1">
+        <span className="text-sm text-gray-500 md:-mb-1">
           /ɪɡ noʊˌbɛlˈpiːdiə/
         </span>
       </div>
       
       {/* Subtitle */}
-      <h3 className="text-2xl mb-4 py-4 font-manrope">
+      <h3 className="text-1xl md:text-2xl mb-4 py-4 font-manrope">
         For achievements that first make people laugh, then think
       </h3>
     </div>
