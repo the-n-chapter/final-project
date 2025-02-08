aac821920d155f59dec36f78ffd749a4628f369a
phamk4 <khanh.b.pham@aalto.fi>
2025-02-08 23:18:04 +0200

    Subtitle h3 typing effect

--- src/components/main-content/Title.tsx
+++ src/components/main-content/Title.tsx
@@ -1,6 +1,7 @@
 import '@fontsource/noto-serif/400.css';
 import '@fontsource/noto-serif/700.css';
 import '@fontsource/manrope/400.css'; // Default weight
+import { ReactTyped } from "react-typed";
 
 export function Title() {
   return (
@@ -17,7 +18,22 @@ export function Title() {
       
       {/* Subtitle */}
       <h3 className="text-1xl md:text-2xl mb-4 py-4 font-manrope">
-        For achievements that first make people laugh, then think
+        <span>{"For achievements that 🌞 "}</span>
+        <ReactTyped
+          strings={[
+          `<span class="text-yellow-500">first make people laugh </span>`,
+          `<span class="text-yellow-500">then make them think</span>`,
+        ]}
+          typeSpeed={60}
+          startDelay={1500}
+          backDelay={2000}
+          backSpeed={30}
+          loop
+          loopCount={Infinity}
+          showCursor
+          cursorChar=" _"
+          className="[&_.typed-cursor]:text-yellow-500" // correct usage to customize cursor
+        />
       </h3>
     </div>
   );
