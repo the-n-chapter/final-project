479451be8639dfe562cee17cc3803b87f904a5b5
phamk4 <khanh.b.pham@aalto.fi>
2025-02-09 14:55:17 +0200

    Update comment for data related utils

--- src/data/excel-reader.ts
+++ src/data/excel-reader.ts
@@ -1,22 +1,24 @@
+// excel-reader.ts
+
 import * as XLSX from 'xlsx';
 
 export const excelRead = (filePath: string) => {
-  // Using fetch to access the file in the public directory
+  // Fetches the file from the given filePath
   return fetch(filePath)
     .then((res) => {
-      if (!res.ok) {
+      if (!res.ok) { // if response is not ok, throw an error
         throw new Error('Failed to fetch file');
       }
-      return res.arrayBuffer();  // Convert the response to ArrayBuffer
+      return res.arrayBuffer();  // Convert response to an ArrayBuffer (binary format required by XLSX)
     })
     .then((data) => {
       const workbook = XLSX.read(data, { type: 'array' });
-      const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-      const jsonData = XLSX.utils.sheet_to_json(worksheet);
-      return jsonData;
+      const worksheet = workbook.Sheets[workbook.SheetNames[0]]; // Extract the 1st worksheet
+      const jsonData = XLSX.utils.sheet_to_json(worksheet); // Convert worksheet to JSON
+      return jsonData; // returns JSON data if success
     })
     .catch((err) => {
-      console.error("Error reading Excel file:", err);
+      console.error("Error reading Excel file:", err); // returns error or empty array if fails
       return [];
     });
 };
--- src/data/winners-data.ts
+++ src/data/winners-data.ts
@@ -1,8 +1,11 @@
-'use client';
+// winners-data.ts
+
+'use client'; // include this because without this, useEffect and useState cannot run (client-side hooks)
 
 import { useEffect, useState } from 'react';
 import { excelRead } from './excel-reader';  
 
+// Define data structure
 export interface IgNobelWinner {
   year: number;
   field: string;
@@ -11,16 +14,17 @@ export interface IgNobelWinner {
 }
 
 const useWinners = () => {
-  const [winners, setWinners] = useState<IgNobelWinner[]>([]);
+  const [winners, setWinners] = useState<IgNobelWinner[]>([]); // empty array at first
+  // use <IgNobelWinner[]> so TypeScript will warn when I use wrong type of data
 
-  useEffect(() => {
-    const filePath = '/data.xlsx';  // This file in 'public' directory
-    excelRead(filePath).then((excelData) => {
-      setWinners(excelData as IgNobelWinner[]);
+  useEffect(() => { // Load only once when we open the browser
+    const filePath = '/data.xlsx';  // Tell the browser where to find the file -> public 
+    excelRead(filePath).then((excelData) => { // Read the file and convert into JSON (JavaScript object)
+      setWinners(excelData as IgNobelWinner[]); // Update 'winners' state with new data  
     });
   }, []);
 
-  return winners;
+  return winners; // Can reuse winners whenever calls userWinners()
 };
 
 export default useWinners;
