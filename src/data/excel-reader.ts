6219a9ac345aca1962e4c784e8718cb9003566f6
phamk4 <khanh.b.pham@aalto.fi>
2025-02-10 02:35:25 +0200

    Optimized excel-reader.ts

--- src/data/excel-reader.ts
+++ src/data/excel-reader.ts
@@ -1,24 +1,19 @@
-// excel-reader.ts
+export const excelRead = async (filePath: string) => {
+  try {
+    // Dynamically import 'xlsx' to reduce bundle size
+    const XLSX = await import('xlsx');
 
-import * as XLSX from 'xlsx';
+    // Fetch the file from the given filePath
+    const res = await fetch(filePath);
+    if (!res.ok) throw new Error('Failed to fetch file');
 
-export const excelRead = (filePath: string) => {
-  // Fetches the file from the given filePath
-  return fetch(filePath)
-    .then((res) => {
-      if (!res.ok) { // if response is not ok, throw an error
-        throw new Error('Failed to fetch file');
-      }
-      return res.arrayBuffer();  // Convert response to an ArrayBuffer (binary format required by XLSX)
-    })
-    .then((data) => {
-      const workbook = XLSX.read(data, { type: 'array' });
-      const worksheet = workbook.Sheets[workbook.SheetNames[0]]; // Extract the 1st worksheet
-      const jsonData = XLSX.utils.sheet_to_json(worksheet); // Convert worksheet to JSON
-      return jsonData; // returns JSON data if success
-    })
-    .catch((err) => {
-      console.error("Error reading Excel file:", err); // returns error or empty array if fails
-      return [];
-    });
+    const data = await res.arrayBuffer(); // Convert response to an ArrayBuffer
+
+    const workbook = XLSX.read(data, { type: 'array' });
+    const worksheet = workbook.Sheets[workbook.SheetNames[0]]; // Extract the 1st worksheet
+    return XLSX.utils.sheet_to_json(worksheet); // Convert worksheet to JSON
+  } catch (err) {
+    console.error("Error reading Excel file:", err);
+    return []; // Return empty array on failure
+  }
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "/final-project",
+  plugins: [react(), visualizer({ open: true })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
